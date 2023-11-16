e27f83eb9430dded3c5383d531d61911fe08e499
WenChenHH <112915016+WenChenHH@users.noreply.github.com>
2023-11-16 11:37:47 +0800

    Update class.py

--- 20231116/class.py
+++ 20231116/class.py
@@ -5,12 +5,16 @@ class Person:
   
   def __str__(self):
     return f"Name: {self.name}, Age = {self.age}"
+  def myfunc(self):
+    print("Hello my name is " + self.name)
     
 
 p1 = Person("John", 36)
 p2 = Person("J.Sheon",32)
-p3= Person(input("請輸入名字:  "),input("請輸入年齡:  "))
+#p3= Person(input("請輸入名字:  "),input("請輸入年齡:  "))
 
 print(p1)
 print("Name:",p2.name,",","Age =",p2.age)
-print(p3)
+print("\n")
+#print(p3)
+p1.myfunc()
