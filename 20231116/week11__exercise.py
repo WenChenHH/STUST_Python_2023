57862274cd51fe63bf18b7e6590093333d3f915e
WenChenHH <112915016+WenChenHH@users.noreply.github.com>
2023-11-17 23:39:26 +0800

    Update week11__exercise.py

--- 20231116/week11__exercise.py
+++ 20231116/week11__exercise.py
@@ -13,13 +13,27 @@ class Myshape:
     def __round__(self):
         area=self.radius**2*3.14
         return print(f"圓形形面積={area}")
-    
+#-----------------用另一種方式寫---------------
+class test :
+    def rectangle(length,width):
+        area = length*width
+        return print(f"長方形面積={area}")
+    def square(side):
+       area=side*side
+       return print(f"正方形面積={area}")
+    def round(radius):
+        area=radius**2*3.14
+        return print(f"圓形形面積={area}")
 E =Myshape(4,3,5,6)
 
 E.__rectangle__()
 E.__square__()
 E.__round__()
 
+A=test
+A.rectangle(3,4)
+A.square(5)
+A.round(6)
     
 
     
