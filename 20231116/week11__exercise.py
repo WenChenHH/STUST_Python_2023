873e25b34ea6d46687e1d0b7ce4bbbdc1cd17bdf
WenChenHH <112915016+WenChenHH@users.noreply.github.com>
2023-11-21 00:17:48 +0800

    Update week11__exercise.py

--- 20231116/week11__exercise.py
+++ 20231116/week11__exercise.py
@@ -1,9 +1,9 @@
 class Myshape:
     def __init__(self,length,width,side,radius):
-        self.length=length
-        self.width=width
-        self.side=side
-        self.radius=radius
+        self.length=int(length)
+        self.width=int(width)
+        self.side=int(side)
+        self.radius=float(radius)
     def __rectangle__(self):
         area = self.length*self.width
         return print(f"長方形面積={area}")
@@ -24,7 +24,7 @@ class test :
     def round(radius):
         area=radius**2*3.14
         return print(f"圓形形面積={area}")
-E =Myshape(4,3,5,6)
+E =Myshape(input("請輸入長(CM):"),input("請輸入寬(CM):"),input("請輸入正方形邊長(CM):"),input("請輸入半徑(CM):"))
 
 E.__rectangle__()
 E.__square__()
@@ -34,7 +34,9 @@ A=test
 A.rectangle(3,4)
 A.square(5)
 A.round(6)
-    
+
+
+
 
     
         
