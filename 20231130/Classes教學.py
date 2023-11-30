fe8c5f23c2c85bfc064df3af4beb27e35a0ee30a
WenChenHH <112915016+WenChenHH@users.noreply.github.com>
2023-11-30 10:34:17 +0800

    classes練習

--- 20231130/Classes教學.py
+++ 20231130/Classes教學.py
@@ -1,3 +1,44 @@
+
+# # 汽車類別
+# class Cars:
+#     # 建構式
+#     def __init__(self, color, seat):
+#         self.color = color  # 顏色屬性
+#         self.seat = seat  # 座位屬性
+# mazda = Cars("blue", 4)
+
+
+
+# # 汽車類別
+# class Cars:
+#     pass
+# # 摩托車類別
+# class Motorcycle:
+#     pass
+# # 建立Cars類別的物件
+# mazda = Cars()
+# mazda.color = "blue"  #顏色屬性 
+# mazda.seat = 4  #座位屬性
+# print(isinstance(mazda, Cars))  # 執行結果：True
+# print(isinstance(mazda, Motorcycle))  # 執行結果：False
+#print(mazda.color)  # 執行結果：blue
+#print(mazda.seat)  # 執行結果：4
+
+
+
+
+
+# # 汽車類別
+# class Cars:
+#     # 建構式
+#     def __init__(self, color, seat):
+#         self.color = color  # 顏色屬性
+#         self.seat = seat  # 座位屬性
+#     # 方法(Method)
+#     def drive(self):
+#         print(f"My car is {self.color} and {self.seat} seats.")
+
+
 #類別
 class cars:
     #建構式
@@ -9,8 +50,5 @@ class cars:
     def drive(self):
         print(f"My car is {self.color} and has {self.seat} seats.")
 
-
-
-mazda = cars() #建立物件類別
-mazda.color = "blue" #顏色屬性
-mazda.seat = 4  #座位屬性
+mazda = cars("Red",5)
+mazda.drive()
---
+++ 20231130/classes練習.py
@@ -0,0 +1,12 @@
+class Student:
+
+    def __init__(self,School,Department,Grade,Name,ID,Credit,Score,School_Addres,student_Addres):
+        self.school=School
+        self.department=Department
+        self.grade=Grade
+        self.credit=Credit
+        self.score=Score
+        self.name=Name
+        self.school_Addres=School_Addres
+        self.student_Addres=student_Addres
+        self.id=ID
