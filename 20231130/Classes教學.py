e9db8c2abb8111c005749936b5c6e884383c2b58
WenChenHH <112915016+WenChenHH@users.noreply.github.com>
2023-11-30 10:06:45 +0800

    Update Classes教學.py

--- 20231130/Classes教學.py
+++ 20231130/Classes教學.py
@@ -1,17 +1,8 @@
-#類別
-class cars:
-    #建構式
-    def __init__(self,color,seat):
-        self.color = color
-        self.seat = seat
+\
 
-    #方法
-    def drive(self):
-        print(f"My car is {self.color} and {self.seat} seats.") 
+mazda = cars() #建立物件類別
+mazda.color = "blue" #顏色屬性
+mazda.seat = 4  #座位屬性
 
-
-
-T1=cars("blue",4)
-
-T1.drive
-
+print(isinstance(mazda,cars))
+print(isinstance(mazda,Motorcycle))
