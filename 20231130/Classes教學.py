8e5ab504e4cc8478f022539154c7da756a0478e3
WenChenHH <112915016+WenChenHH@users.noreply.github.com>
2023-11-30 09:48:09 +0800

    Update Classes教學.py

---
+++ 20231130/Classes教學.py
@@ -0,0 +1,15 @@
+#類別
+class cars:
+    #建構式
+    def __init__(self,color,seat):
+        self.color = color
+        self.seat = seat
+
+    #方法
+    def drive(self):
+        print(f"My car is {self.color} and {self.seat} seats.") 
+
+
+
+T1=cars("Blue",4)
+
