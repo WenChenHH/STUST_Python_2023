286c8315cd2f7bc99c306c8b1d3689d33718f995
WenChenHH <112915016+WenChenHH@users.noreply.github.com>
2024-01-04 10:46:03 +0800

    Update stust.py

--- 20240104/stust.py
+++ 20240104/stust.py
@@ -54,23 +54,58 @@ class HotDrink(Drink):#熱飲
 
 # 給一個stust_csie_drink的參數
 employee1 = stust_csie_drink("John",5,80)
+employee2 = stust_csie_drink("David",10,90)
+employee3 = stust_csie_drink("Lisa",2,50)
 
 # 給一個 ColdDrink 的參數
 cold_drink1 = ColdDrink("冰紅茶","少冰","中糖")
+cold_drink2 = ColdDrink("冰綠茶","去冰","少糖")
+hot_drink1 = HotDrink("熱奶茶","去冰","微糖")
 
 # 將飲料指派給員工
 employee1.assign_drink(cold_drink1)
+employee2.assign_drink(cold_drink2)
+employee3.assign_drink(hot_drink1)
 
 # 列印員工資訊和所選擇的飲料資訊
 print(f"員工姓名：{employee1.query_name()}")
 print(f"員工年資：{employee1.query_seniority()} 年")
 print(f"員工工時：{employee1.query_work_hours()} 小時")
 print(f"員工月薪：{employee1.calculate_monthly_salary()} 元")
+print("")
+print(f"員工姓名：{employee2.query_name()}")
+print(f"員工年資：{employee2.query_seniority()} 年")
+print(f"員工工時：{employee2.query_work_hours()} 小時")
+print(f"員工月薪：{employee2.calculate_monthly_salary()} 元")
+print("")
+print(f"員工姓名：{employee3.query_name()}")
+print(f"員工年資：{employee3.query_seniority()} 年")
+print(f"員工工時：{employee3.query_work_hours()} 小時")
+print(f"員工月薪：{employee3.calculate_monthly_salary()} 元")
 
 if employee1.drink is not None:
     print("\n所選擇的飲料資訊:")
     print(f"飲料名稱：{employee1.drink.name}")
     print(f"冰量：{employee1.drink.ice_level}")
     print(f"甜度：{employee1.drink.sweetness}")
+    print("")
+else:
+    print("\n員工尚未選擇飲料。")
+    
+if employee2.drink is not None:
+    print("\n所選擇的飲料資訊:")
+    print(f"飲料名稱：{employee2.drink.name}")
+    print(f"冰量：{employee2.drink.ice_level}")
+    print(f"甜度：{employee2.drink.sweetness}")
+    print("")
+else:
+    print("\n員工尚未選擇飲料。")
+
+if employee3.drink is not None:
+    print("\n所選擇的飲料資訊:")
+    print(f"飲料名稱：{employee3.drink.name}")
+    print(f"冰量：{employee3.drink.ice_level}")
+    print(f"甜度：{employee3.drink.sweetness}")
+    print("")
 else:
     print("\n員工尚未選擇飲料。")
